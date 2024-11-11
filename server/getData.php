6e88c53f1c55d7501ec269e67ebf87e2cc84bd46
Dorian-Cesar <dorian.celu@gmail.com>
2024-11-11 13:09:30 -0300

    fix mapa

--- server/getData.php
+++ server/getData.php
@@ -1,4 +1,14 @@
 <?php
+// Permitir solicitudes desde tu dominio específico (Netlify)
+header("Access-Control-Allow-Origin: https://certificado-masgps.netlify.app");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Manejar la solicitud preflight (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header("HTTP/1.1 204 No Content");
+    exit;
+}
 
 require './config.php';
 
