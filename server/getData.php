2e83bddc6c9139adadad01adb543e7200071802a
Dorian-Cesar <dorian.celu@gmail.com>
2024-11-11 12:26:34 -0300

    fase 2

---
+++ server/getData.php
@@ -0,0 +1,49 @@
+<?php
+
+require './config.php';
+
+// Verificar que la solicitud sea POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Decodificar el JSON del cuerpo de la solicitud
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    // Validar que se haya enviado el valor de la patente
+    if (!isset($input['patente']) || empty($input['patente'])) {
+        http_response_code(400);
+        echo json_encode(['error' => 'Patente es requerida']);
+        exit();
+    }
+
+    $patente = $input['patente'];
+  
+
+    // Preparar la consulta SQL
+    $query = "SELECT * FROM masgps.LpfPullmanInfoView WHERE patente = '$patente'";
+
+    // Ejecutar la consulta
+    $result = $conn->query($query);
+
+    // Verificar si se obtuvo algún resultado
+    if ($result && $result->num_rows > 0) {
+        // Obtener el resultado en un array asociativo
+        $data = $result->fetch_assoc();
+        
+        // Devolver el resultado en formato JSON
+        echo json_encode($data);
+    } else {
+        // Si no se encuentra el vehículo
+        http_response_code(404);
+        echo json_encode(['error' => 'Vehículo no encontrado']);
+    }
+
+    // Liberar el resultado
+    $result->free();
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Método no permitido']);
+}
+
+// Cerrar la conexión
+$conn->close();
+?>
+
